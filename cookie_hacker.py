8f76cb5b340f8a5cd590068a2e1cb69cb77a07af
farliy-hacker <62786635+farliy-hacker@users.noreply.github.com>
2020-10-07 22:32:55 +0800

    Create cookie_hacker.py

---
+++ cookie_hacker.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import random
+
+def hacker_kill():
+         cookie = [
+              {'name1':'1234'},
+              {'name2':'123456'},
+              {'name3':'12345678'},
+              {'name4':'123456789'}
+         ]
+         
+
+          return random.choice(cookie)
